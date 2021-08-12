d5eed98da6785d95be85e987c8b3d2d0214eeb60
mithulp <mithul.p66@gmail.com>
2021-08-12 14:04:41 +0530

    new push

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,54 @@
+package org.emp;
+
+public class Employee {
+
+	
+	
+	public Employee(String name) {
+		this('a'); System.out.println("mithul : "+name);
+	
+	}
+	public Employee(char block) {
+	System.out.println("a"+block);
+	}
+
+	public Employee() {
+		this("mk"); System.out.println("defaultconstructor");
+	}
+	
+	private void empid() {
+		System.out.println("emp name is : mithul");
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Employee e = new Employee();
+		e.empid();
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
